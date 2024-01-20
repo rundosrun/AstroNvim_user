2d90baf0ec1be4aefebb1bcfaf0a03f46132b1f9
Vitaly Grachev <gravitazione@yandex.ru>
2024-01-20 21:45:36 +0300

    Add transparency

--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -1,3 +1,4 @@
+local prefix = "<leader>u"
 return {
   {
     "folke/tokyonight.nvim",
@@ -7,5 +8,55 @@ return {
       -- load the colorscheme here
       vim.cmd [[colorscheme tokyonight-storm]]
     end,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      -- table: default groups
+      groups = {
+        "Comment",
+        "Conditional",
+        "Constant",
+        "CursorLineNr",
+        "EndOfBuffer",
+        "Function",
+        "Identifier",
+        "LineNr",
+        "NonText",
+        "Normal",
+        "NormalNC",
+        "Operator",
+        "PreProc",
+        "Repeat",
+        "SignColumn",
+        "Special",
+        "Statement",
+        "String",
+        "Structure",
+        "Todo",
+        "Type",
+        "Underlined",
+      },
+      -- table: additional groups that should be cleared
+      extra_groups = {
+        "NeoTreeFloatBorder",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NvimTreeNormal",
+        "StatusLine",
+        "StatusLineNC",
+        "BufferLineFill",
+        "BufferLineBackground",
+        "BufferLineBufferSelected",
+      },
+      -- table: groups you don't want to clear
+      exclude_groups = {
+        "NormalFloat",
+      },
+    },
+    keys = {
+      { prefix .. "T", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
+    },
   },
 }
