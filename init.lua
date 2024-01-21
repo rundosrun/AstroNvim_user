9918ced5ef243f19b78c585359994f324fb5b144
Vitaly Grachev <gravitazione@yandex.ru>
2024-01-21 12:22:55 +0300

    Adjust default color scheme catppuccin

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "tokyonight-storm",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -1,15 +1,6 @@
 local prefix = "<leader>u"
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd [[colorscheme tokyonight-storm]]
-    end,
-  },
-  {
     "xiyaowong/transparent.nvim",
     lazy = false,
     opts = {
